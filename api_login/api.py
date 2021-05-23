9b6b61eeb3869949a5c5f4994f4ae6efdad9b7e9
LucasFerreira14 <ferreira.silva5641@gmail.com>
2021-05-23 20:51:20 -0300

    update lucao

--- api_login/api.py
+++ api_login/api.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import login_user, logout_user
 
 # Criação da engine
 engine = create_engine("sqlite:///login.db")
@@ -20,10 +21,10 @@ class Login(Base):
 
     def __init__(self, login, password):
         self.login = login
-        self.password = password
+        self.password = generate_password_hash(password)
     
     def verify_passoword(self, pwd):
-        return check_password_hash(self.senha, pwd)
+        return check_password_hash(self.password, pwd)
 
 
 # Cria o banco
@@ -46,16 +47,30 @@ def criaUser(user, pwd):
     session.commit()
     session.close()
 
-def validaUser(user, pwd):
+def loginUser(user, pwd):
     search = session.query(Login).filter_by(login=user).first()
 
-    if user == search.login:
-        if pwd == search.password:
-            return True
-        else:
-            return False
+    if not search or not search.verify_passoword(pwd):
+        session.close()
+        return False
+    else:
+        session.close()
+        return [True, search.login]
+
+def validaUser(user):
+    search = session.query(Login).filter_by(login=user).first()
+    
+    if search == None:
+        return True
     else:
         return False
+
+def deletaUser(user):
+    session.delete(session.query(Login).filter_by(login=user).first())
+
+    session.commit()
+    session.close()
+
 
 
 
--- app/model/tables.py
+++ app/model/tables.py
@@ -6,19 +6,17 @@ class Usuarios(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(150))
     sobrenome = db.Column(db.String(150))
-    user_name = db.Column(db.String(16), unique=True)
+    username = db.Column(db.String(16))
     telefone = db.Column(db.String(14))
     email = db.Column(db.String(255), unique=True)
-    senha = db.Column(db.String(150))
     created_at = db.Column(db.String(120))
     updated_at = db.Column(db.String(120))
 
-    def __init__(self, nome, sobrenome, user_name, telefone, email, senha, created_at, updated_at):
+    def __init__(self, nome, sobrenome, username, telefone, email, created_at, updated_at):
         self.nome = nome
         self.sobrenome = sobrenome
-        self.user_name = user_name
+        self.username = username
         self.telefone = telefone
         self.email = email
-        self.senha = senha
         self.created_at = created_at
         self.updated_at = updated_at
