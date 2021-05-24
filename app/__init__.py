0a1ccae137a45f5871886cbee755d8b0612d88a5
LucasFerreira14 <ferreira.silva5641@gmail.com>
2021-05-24 19:38:34 -0300

    update lucao

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_login import LoginManager, login_manager, UserMixin
 app = Flask(__name__, template_folder="templates")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///usuario.db"
 app.config["SECRET_KEY"] = "secret"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 
